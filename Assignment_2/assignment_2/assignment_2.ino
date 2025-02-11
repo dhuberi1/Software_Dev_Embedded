8f050cbf65ce07242c7bcc93d4ea669e3facdc2a
Josh Choe <kirbyfan19@gmail.com>
2025-02-11 18:52:41 -0500

    Added the ability to store temperture and time readings. Added the ability to  dump data over serial when signal is sent to appropriate pin.

--- Assignment_2/assignment_2/assignment_2.ino
+++ Assignment_2/assignment_2/assignment_2.ino
@@ -6,6 +6,7 @@ FspTimer temp_timer;
 
 // Analog input pin
 int temp_pin = A0;
+int send_data_pin = 2;
 
 // Refernce voltage (V)
 float v_ref = 5;
@@ -13,8 +14,21 @@ float v_ref = 5;
 // Rate of the timer interrupt (Hz)
 float rate = 1;
 
-float volatile f;
+
+// Stores temperture and time into arrays to later be sent over serial
+float volatile tempF[1000];
+unsigned long volatile msec[1000];
+int idx = 0;
+
+// Count for 10s delay
 int count = 0;
+
+// Flag for sending data
+bool sendData = false;
+
+// Debounce parameters for button
+unsigned long debounceTime = 0;
+unsigned long debounceDelay = 500;
 
 // Temp_sense object
 Temp_Sense temp = Temp_Sense(temp_pin, v_ref);
@@ -28,8 +42,30 @@ Runs readTemp() function.
 void temp_timer_callback(timer_callback_args_t __attribute((unused)) *p_args) {
   count++;
 
+  // Timer callback is called every 1s, ensures data is recorded every 10s 
+  // Arduino does not seem to be able to have timer interrupts at 0.1 hz natively
   if (count % 10 == 0) {
-    f = temp.readTemp();
+    if (idx < 1000) {
+      tempF[idx] = temp.readTemp();
+      msec[idx] = millis();
+
+      idx++;
+
+      // resets count
+      count = 0;
+    }
+  }
+}
+
+/*
+send_data_ISR
+
+Send data button interrupt.
+Sets sendData to true if button has been pressed. Includes debounce protection.
+*/
+void send_data_ISR() {
+  if ((millis() - debounceTime) > debounceDelay) {
+    sendData = true;
   }
 }
 
@@ -40,7 +76,7 @@ from: https://www.pschatzmann.ch/home/2023/07/01/under-the-hood-arduino-uno-r4-t
 Function to set up the timer.
 */
 bool beginTimer(float rate) {
-  uint8_t timer_type = GPT_TIMER;
+  uint8_t timer_type = AGT_TIMER;
   int8_t t_index = FspTimer::get_available_timer(timer_type);
 
   if (t_index < 0) {
@@ -73,30 +109,38 @@ void setup() {
   // Opens Serial on 9600 baud rate
   Serial.begin(9600);
 
-  // Sets pin A0 to an input
+  // Sets pin temp_pin to an input
   pinMode(temp_pin, INPUT);
+
+  // Sets send_data_pin to a input pullup and attaches a falling edge interrupt to it.
+  pinMode(send_data_pin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(send_data_pin), send_data_ISR, FALLING);
 
   // Setsup and begins timer
   beginTimer(rate);
 
   // Print a CSV Header 
-  Serial.println("Time in ms (ms), Temp (F)")
+  Serial.println("Time in ms (ms), Temp (F)");
   
 }
 
 void loop() {
-  // avoiding shared data with reading potential issues 
-  noInterrupts(); 
-  float tempCopy = f; 
-  interrupts();
+  // Checks if sendData flag is true
+  if (sendData == true){
+    // Dumps collected data over serial
+    for (int i = 0; i < sizeof(tempF); i++) {
+      if (msec[i] != 0){
+        Serial.print(msec[i]); 
+        Serial.print(","); 
+        Serial.println(tempF[i]);
+      }
+      else {
+        Serial.println("DATA SENT!");
+        break;
+      }
+    }
 
-  // Now we need to get associated timestamp 
-  unsigned long currentTime = millis(); 
-
-  // Print CSV formatted output to Serial for now 
-  Serial.print(currentTime); 
-  Serial.print(","); 
-  Serial.println(tempCopy); 
-  // Serial.println(f);
-  delay(1000); // wait a secnond before next read 
+    // Resets sendData flag
+    sendData = false;
+  }
 }
