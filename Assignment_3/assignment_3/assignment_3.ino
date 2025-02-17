e70f696ab1a11c469d3f95194b610a4a46a3ab1d
Josh Choe <kirbyfan19@gmail.com>
2025-02-17 15:45:44 -0500

    Added interrupt for IR detection

--- Assignment_3/assignment_3/assignment_3.ino
+++ Assignment_3/assignment_3/assignment_3.ino
@@ -1,17 +1,26 @@
-int IR_pin = A0;
+// IR detector pin
+int IR_pin = 2;
 
-//int IR_thresh = ;
+// Records number of times and the time when the prop blade passes between the IR sensors 
+int count = 0;
+int msec[1000];
+
+void IR_detect() {
+  if (count < sizeof(msec)) {
+    msec[count] = millis();
+    count++;
+  }
+}
 
 void setup() {
   // Opens Serial on 9600 baud rate
   Serial.begin(9600);
 
-  // Sets pin temp_pin to an input
+  // Sets pin IR_pin to an input and attaches a rising edge interrupt to it
   pinMode(IR_pin, INPUT);
-
+  attachInterrupt(digitalPinToInterrupt(IR_pin), IR_detect, RISING);
 }
 
 void loop() {
-  int IR_detect = analogRead(IR_pin);
-  Serial.println(IR_detect);
+  Serial.println(count);
 }
