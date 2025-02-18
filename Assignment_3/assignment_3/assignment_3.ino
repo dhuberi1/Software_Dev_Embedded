3c724c96543af97d0f0a6d4df8521aad91cbc7e9
Josh Choe <kirbyfan19@gmail.com>
2025-02-18 17:03:36 -0500

    Added logic for powering propeller

--- Assignment_3/assignment_3/assignment_3.ino
+++ Assignment_3/assignment_3/assignment_3.ino
@@ -1,5 +1,12 @@
+#include <Servo.h>
+
+// Servo object
+Servo esc;
+int servo_pin = 3;
+
 // IR detector pin
 int IR_pin = 2;
+int throttle_pin = A0;
 
 // Records number of times and the time when the prop blade passes between the IR sensors 
 int count = 0;
@@ -19,8 +26,18 @@ void setup() {
   // Sets pin IR_pin to an input and attaches a rising edge interrupt to it
   pinMode(IR_pin, INPUT);
   attachInterrupt(digitalPinToInterrupt(IR_pin), IR_detect, RISING);
+
+  // Sets throttle_pin to input and attaches esc to servo_pin
+  pinMode(throttle_pin, INPUT);
+  esc.attach(servo_pin);
 }
 
 void loop() {
-  Serial.println(count);
+  //Serial.println(count);
+
+  int throttle = analogRead(throttle_pin);
+  throttle = map(throttle, 0, 1023, 0, 179);
+
+  esc.write(throttle);
+  Serial.println(throttle);
 }
