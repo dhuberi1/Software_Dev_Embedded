c0c06ca2c14a1e46adee59ac6ec96ffc470e1c80
Josh Choe <kirbyfan19@gmail.com>
2025-04-02 16:37:05 -0400

    Added Assignment 7 code

---
+++ Assignment_7/GPS_Stream.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+import gpsd
+
+IP = "192.168.1.28" # Replace with the IP of the Raspberry Pi
+PORT = 2947 # Replace with the desired port
+
+gpsd.connect(host=IP, port=PORT)
+
+# Get GPS position and altitude
+while True:
+    packet = gpsd.get_current()
+    #print(packet.position())
+    print("Lat:", packet.lat, " Lon:", packet.lon, " Alt:", packet.alt)
