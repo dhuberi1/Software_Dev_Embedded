05a92fac6b3a3c9d468c24b388da3bf78386b88c
Josh Choe <kirbyfan19@gmail.com>
2025-04-11 21:51:51 -0400

    Added ability to send IMU data from UDP socket. Added script for host that receives data from UDP socket.

---
+++ Assignment_8/imu_stream.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import socket
+
+### UDP Socket ###
+UDP_IP = "192.168.1.229" # IP ADDRESS OF HOST DEVICE
+UDP_PORT = 9001
+
+imu_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+imu_socket.bind((UDP_IP, UDP_PORT))
+
+while True:
+    try:
+        data, addr = imu_socket.recvfrom(1024) # buffer size is 1024 bytes
+        data = data.decode()
+        print("received message: %s" % data)
+
+    except KeyboardInterrupt as e: 
+        print("Exiting...")
+        break
--- Assignment_8/log_imu.py
+++ Assignment_8/log_imu.py
@@ -2,15 +2,23 @@
 import serial
 import serial.tools.list_ports
 import time
+import socket
 
-# Set our variables
+# VARIABLES
+### Arduino Serial ###
 SERIAL_PORT = "/dev/ttyACM0"
 BAUD_RATE = 115200
 
+### UDP Socket ###
+UDP_IP = "192.168.1.28" # IP ADDRESS OF HOST DEVICE
+UDP_PORT = 9001
+
+# Check Device Ports
 ports = serial.tools.list_ports.comports()
 for port, desc, hwid in sorted(ports):
     print("{}: {} [{}]".format(port, desc, hwid))
 
+# Connect to serial
 try:
     ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
     time.sleep(2) # in case it takes time to establish a connection
@@ -18,10 +26,21 @@ try:
 except Exception as e:
     print(e)
 
+# Connect to UDP socket
+try:
+    imu_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+except Exception as e:
+    print(e)
+
+
 while True:
     try:
-         if ser.in_waiting > 0:
-              data = ser.readline().decode('utf-8').rstrip()
-              print(data)
+        if ser.in_waiting > 0:
+            data = ser.readline().decode('utf-8').rstrip()
+            print(data)
+
+            imu_socket.sendto(data.encode('utf-8'), (UDP_IP, UDP_PORT))
+        
     except Exception as e:
-         print(e)
+        print(e)
